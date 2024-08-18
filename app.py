b4a11dda3fb83063d7a7fe0688672d389edbabb7
pwskills123 <md.a@pw.live>
2024-08-18 20:54:31 +0530

    first commitok

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template, jsonify, request, send_file
 from src.exception import CustomException
 from src.logger import logging as lg
 import os,sys
-from urllib.parse import quote as url_quote
 
 
 
+#just
 from src.pipeline.train_pipeline import TraininingPipeline
 from src.pipeline.predict_pipeline import PredictionPipeline
 
