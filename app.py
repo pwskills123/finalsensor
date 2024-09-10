92c71e9a54ca421da6208c3e0482a96f78753ff8
pwskills123 <md.a@pw.live>
2024-09-10 22:44:31 +0530

    complted message

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, jsonify, request, send_file
 from src.exception import CustomException
 from src.logger import logging as lg
 import os,sys
-from urllib.parse import quote as url_quote
+
 
 
 #just
